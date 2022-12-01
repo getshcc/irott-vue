0273789fcd0ee6a9259eaf48f90a395406b25bae
ShCc <iamshcc@gmail.com>
2022-12-01 14:22:55 +0330

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <section>
+
+    <header>
+    
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+  
+    </header>
+  
+    <main>
+      <RouterView />
+    </main>
+
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+section {
+  padding: 20px;
+}
+
+nav {
+  padding-bottom: 16px;
+
+  a {
+    margin-right: 14px;
+  }
+}
+
+</style>
